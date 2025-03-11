8a4e29c6231560a09846d76dbcab61df2a8caf26
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-11 16:18:04 +0100

    Initialize Polaflix Pablo project with Spring Boot setup and Maven configuration

---
+++ polaflix-pablo/src/main/java/es/unican/polaflix_pablo/PolaflixPabloApplication.java
@@ -0,0 +1,13 @@
+package es.unican.polaflix_pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaflixPabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaflixPabloApplication.class, args);
+	}
+
+}
---
+++ polaflix-pablo/src/test/java/es/unican/polaflix_pablo/PolaflixPabloApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.polaflix_pablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixPabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
