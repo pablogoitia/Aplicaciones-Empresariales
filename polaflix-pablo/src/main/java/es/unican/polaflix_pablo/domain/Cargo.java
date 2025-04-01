a13627745d7e923ba61b7c886e58c0a5e3dec516
Pablo Goitia <pablogoitia01@gmail.com>
2025-04-01 17:32:12 +0200

    Añadido el ID de persistencia

--- polaflix-pablo/src/main/java/es/unican/polaflix_pablo/domain/Cargo.java
+++ polaflix-pablo/src/main/java/es/unican/polaflix_pablo/domain/Cargo.java
@@ -4,13 +4,20 @@ import java.util.Date;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Cargo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     @ManyToOne
     private final Factura factura;
-    
+
     private final Date fechaCargo = new Date();
     private final String nombreSerie;
     private final String capituloTemporada;
