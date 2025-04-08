a409b333d39053e85719d9b18ec647482d110fb3
Pablo Goitia <pablogoitia01@gmail.com>
2025-04-08 18:56:12 +0200

    Corregido error de propagación en las Facturas

--- polaflix-pablo/src/main/java/es/unican/polaflix_pablo/domain/Usuario.java
+++ polaflix-pablo/src/main/java/es/unican/polaflix_pablo/domain/Usuario.java
@@ -41,7 +41,8 @@ public class Usuario {
     // Informacion de facturacion
     private boolean tieneCuotaFija = false;
     
-    @OneToMany(mappedBy = "usuario")
+    @OneToMany(mappedBy = "usuario", cascade={CascadeType.PERSIST, CascadeType.MERGE,
+        CascadeType.REFRESH, CascadeType.DETACH})
     private final List<Factura> facturas = new ArrayList<>();
 
     /**
