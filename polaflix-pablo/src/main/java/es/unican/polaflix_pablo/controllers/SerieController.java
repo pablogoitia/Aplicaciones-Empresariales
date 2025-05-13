7863c4cd888adc713a3953c4ab8f44e5b96f22e6
Pablo Goitia <pablogoitia01@gmail.com>
2025-05-13 20:37:19 +0200

    Actualización para el CORS

--- polaflix-pablo/src/main/java/es/unican/polaflix_pablo/controllers/SerieController.java
+++ polaflix-pablo/src/main/java/es/unican/polaflix_pablo/controllers/SerieController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import es.unican.polaflix_pablo.service.Views;
 
 @RestController
 @RequestMapping("/series")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SerieController {
 
     @Autowired
--- polaflix-pablo/src/main/java/es/unican/polaflix_pablo/controllers/UsuarioController.java
+++ polaflix-pablo/src/main/java/es/unican/polaflix_pablo/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,6 +25,7 @@ import es.unican.polaflix_pablo.service.Views;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
     @Autowired
